8f1a79f1cd93eaf5ac5b745b868585d3b9714d00
mzy <mady_stn@yahoo.com>
2021-05-10 22:11:31 +0300

    it took too long...

---
+++ MotherConsole/Binar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            List<int> binaryNumber = new List<int>();
+
+            do {
+                var digit = number % 2;
+                
+                binaryNumber.Add(digit);
+                number = number / 2;
+            } while (number != 0);
+
+            binaryNumber.Reverse();
+
+            foreach (int i in binaryNumber)
+            {
+                Console.Write(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MotherConsole/LargerThanHundred/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargerThanHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+            do
+            {
+                Console.Write("Insert number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number <= 100)
+                {
+                    Console.WriteLine(number + " is smaller than 100");
+                }
+                else
+                {
+                    Console.WriteLine("The number you have inserted is bigger than 100");
+                }
+                    
+            } while (number <= 100);
+            Console.ReadLine();
+        }
+    }
+}
