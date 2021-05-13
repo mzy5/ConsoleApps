c2542c241ad30a4f53ab071e7899feac95da25b7
mzy <mady_stn@yahoo.com>
2021-05-13 21:09:45 +0300

    sad strings

---
+++ MotherConsole/ConsVoc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsVoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vocale= {"a", "e", "i", "o", "u"};
+            Console.WriteLine("Insert word: ");
+            string word = Console.ReadLine();
+
+            int j = 0;
+            int k = 0; ;
+
+            for (int i = 0; i <= word.Length; i++)
+            {
+                if (word[i].Equals(vocale[i]))
+                {
+                    j++;
+                }
+                else k++;
+            }
+
+            Console.WriteLine($"Nr de vocale: {j}");
+            Console.WriteLine($"Nr de consoane: {k}");
+        }
+    }
+}
