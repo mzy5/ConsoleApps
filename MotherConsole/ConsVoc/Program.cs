a8fa6ce31da952c38c1ec1fa1e155958c88b671d
mzy <mady_stn@yahoo.com>
2021-05-16 18:58:50 +0300

    sad face

--- MotherConsole/ConsVoc/Program.cs
+++ MotherConsole/ConsVoc/Program.cs
@@ -10,24 +10,26 @@ namespace ConsVoc
     {
         static void Main(string[] args)
         {
-            string[] vocale= {"a", "e", "i", "o", "u"};
+            HashSet<char> vocale = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+            int totalV = 0;
+            int totalC = 0; ;
+
             Console.WriteLine("Insert word: ");
             string word = Console.ReadLine();
 
-            int j = 0;
-            int k = 0; ;
-
-            for (int i = 0; i <= word.Length; i++)
+            for (int i = 0; i < word.Length; i++)
             {
-                if (word[i].Equals(vocale[i]))
+                if (vocale.Contains(word[i]))
                 {
-                    j++;
+                    totalV++;
                 }
-                else k++;
+                else totalC++;
             }
 
-            Console.WriteLine($"Nr de vocale: {j}");
-            Console.WriteLine($"Nr de consoane: {k}");
+            Console.WriteLine($"Nr de vocale: {totalV}");
+            Console.WriteLine($"Nr de consoane: {totalC}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ MotherConsole/Invers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert word to mirror: ");
+            string word = Console.ReadLine();
+
+            int i = 0; // first element of the reversed part
+            int j = word.Length - 1; // last element of the reversed part
+
+            while (i < j)
+            {
+                var temp = word[i];
+                word[i] = word[j];
+                word[j] = temp;
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
