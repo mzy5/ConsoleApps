07dd0ab4d16704d7bfb4122da793b5f4f84596e1
mzy <mady_stn@yahoo.com>
2021-05-17 20:58:11 +0300

    no words

---
+++ MotherConsole/Palindrom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in word: ");
+            string word = Console.ReadLine();
+            int j = 0;
+
+            for (int i = 0; i <= word.Length-1; i++)
+            {
+                if (word[i].Equals(word[word.Length-1]))
+                {
+                    j++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
