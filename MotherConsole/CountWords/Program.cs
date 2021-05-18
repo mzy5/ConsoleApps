df13c93dc7cec95a6edfc0f047b926f1307856aa
mzy <mady_stn@yahoo.com>
2021-05-18 21:59:04 +0300

    hmmm lookin' good

---
+++ MotherConsole/CountWords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert text: ");
+            string text = Console.ReadLine();
+
+            char empty = ' ';
+            int counter = 1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == empty && text[i+1] != empty)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"There are {counter} words in this text. ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MotherConsole/ReplaceWithUnderline/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceWithUnderline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert space separated words: ");
+            string textCuSpatii = Console.ReadLine();
+            var arr = textCuSpatii.ToCharArray();
+
+            char empty = ' ';
+
+            for (int i = 0; i < textCuSpatii.Length; i++)
+            {
+                if(textCuSpatii[i] == empty)
+                {
+                    arr[i] = '_'; // wip
+                }
+            }
+        }
+    }
+}
