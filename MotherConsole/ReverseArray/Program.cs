11fbb3fd02ce911aaaaff7dc8cbb980414cfedea
mzy5 <mady_stn@yahoo.com>
2021-06-03 20:16:00 +0300

    added stuff

--- MotherConsole/ReverseArray/Program.cs
+++ MotherConsole/ReverseArray/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ReverseArray
+namespace Arrays
 {
     class Program
     {
@@ -8,6 +8,7 @@ namespace ReverseArray
         {
             int[] arrayOfNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
+            //reverse array
             int i = 0; // first element of the reversed part
             int j = arrayOfNumbers.Length - 1; // last element of the reversed part
 
@@ -20,13 +21,44 @@ namespace ReverseArray
                 i++;
                 j--;
             }
-
+            Console.WriteLine("The reversed vector is:");
             for (int k = 0; k < arrayOfNumbers.Length; k++)
             {
                 Console.Write(arrayOfNumbers[k] + " ");
             }
 
+            //check and count odd numbers
+            int numberOfOddNumbers = 0;
+            for (int l = 0; l < arrayOfNumbers.Length; l++)
+            {
+                if(arrayOfNumbers[l] % 2 != 0)
+                {
+                    numberOfOddNumbers++;
+                }
+            }
+            Console.WriteLine($"\nThere are {numberOfOddNumbers} odd numbers");
+
+            //display items on even and odd positions
+            Console.WriteLine("The numbers on the even positions are: ");
+            for (int m = 0; m < arrayOfNumbers.Length; m++)
+            {
+                if (m % 2 == 0)
+                {
+                    Console.Write(arrayOfNumbers[m]);
+                }
+            }
+            Console.WriteLine("\nThe numbers on the odd positions are: ");
+            for (int n = 0; n < arrayOfNumbers.Length; n++)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.Write(arrayOfNumbers[n]);
+                }
+            }
+
             Console.ReadKey();
         }
+
+        
     }
 }
